28e9c9b72ea16e8d4193e4ad394ef257eb8b4acf
Henrique <henrique.prosa@hotmail.com>
2021-05-30 11:31:21 -0300

    feat: added swagger :tada:

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
 import { app } from ".";
 
-app.listen(3333, () => console.log("Server is running!"));
+import path  from 'path';
+import swaggerUi from "swagger-ui-express"
+// import swaggerFile from "./swagger.json"
+import YAML from "yamljs"
+
+const swagger_path =  path.resolve(__dirname,'./swagger.yml');
+console.log(swagger_path)
+const swaggerFile = YAML.load(swagger_path)
+
+const PORT = 3333
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.listen(PORT, () => console.log("Server is running on PORT: " + PORT + "!"));
